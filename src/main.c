ae282a4dd5796cbbf6f8ba680c27a2c413ac58f5
Christoff Humphries <christoff@deadbeef.monster>
2024-05-24 22:27:54 -0500

    simple changes

--- src/main.c
+++ src/main.c
@@ -1,6 +1,31 @@
+#include <assert.h>
+#include <limits.h>
+#include <locale.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+/* Include stack buffer pool code */
+/* #include "buffer_pool.h" */
+
+
 
 int main() {
-    printf("Hello, World!\n");
+    /* Initialize buffer pools */
+
+    /* Print header */
+    printf("Azalyx 0.1-development - A C2 written using C with JPL coding standard restrictions.\n");
+
+    /* Parse arguments */
+    /* Parse configuration file */
+
+    /* Initialize stuff based on configuration */
+
+    /* Start service(s) */
+
+    /* Save state */
+
+    /* Clean up */
     return 0;
 }
