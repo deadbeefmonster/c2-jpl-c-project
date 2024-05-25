0a9b64ff55731cf1079140fe42806c60b782a5b1
Christoff Humphries <christoff@deadbeef.monster>
2024-05-25 05:02:42 -0500

    Compile with all warnings and fix the issues that came up

--- src/main.c
+++ src/main.c
@@ -1,3 +1,6 @@
+/*
+ * Standard includes
+ */
 #include <assert.h>
 #include <limits.h>
 #include <locale.h>
@@ -6,21 +9,32 @@
 #include <string.h>
 #include <time.h>
 
+/*
+ * Networking includes
+ */
+#include <arpa/inet.h>
+#include <unistd.h>
+
+/*
+ * This application's includes
+ */
 /* Include stack buffer pool code */
 /* #include "buffer_pool.h" */
+#include "main.h"
 
-
-
-int main() {
-    /* Initialize buffer pools */
-
+int main(int argc, char *argv[]) {
     /* Print header */
     printf("Azalyx 0.1-development - A C2 written using C with JPL coding standard restrictions.\n");
 
-    /* Parse arguments */
+    /* Initialize buffer pools */
+
+    /* Parse command arguments */
+    for (int i = 0; i < argc && i < 20; i++) {
+        printf("Argument %d: %s\n", i + 1, argv[i]);
+    }
     /* Parse configuration file */
 
-    /* Initialize stuff based on configuration */
+    /* Initialize variables/state based on configuration */
 
     /* Start service(s) */
 
---
+++ src/main.h
@@ -0,0 +1,8 @@
+
+#ifndef AZALYX_MAIN_H
+#define AZALYX_MAIN_H
+
+int main (int argc, char *argv[]);
+
+
+#endif //AZALYX_MAIN_H
